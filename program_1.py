2866cf78068ec85063f4157229e7fd1e2246f3c6
faithsunxngel-sketch <faithsunxngel@gmail.com>
2025-10-06 12:35:45 -0500

    Week 5 programs
    Week 5 programs, the first one is a kilometer conversion and the second one is a simple math quiz

--- program_1.py
+++ program_1.py
@@ -6,21 +6,18 @@
 
 
 def kilometer_conversion(kilometers):    
-    miles = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################    
+    miles = kilometers * 0.6214
+    return miles 
 
 
-    # Return the variable to the calling function
-    return miles
-
-#### This piece of the code has been done for you,
-#### you only need to worry about the actual kilometer
-#### conversion logic in the kilometer_conversion function
 if __name__ == '__main__':
     # Get User Input
     print('in main')
-    # Call kilometer_conversion, don't forget to pass in the kilometer parameter!
-    
-    # Display the miles
+    km = float(input("Enter a distance in kilometers: "))
+
+    # Call the function
+    miles = kilometer_conversion(km)
+
+    # Display result
+    print(f"{km} kilometers is equal to {miles:.2f} miles.")
+
--- program_2.py
+++ program_2.py
@@ -11,3 +11,22 @@
 # If the answer is correct, a message of congratulations should be displayed.  
 # If the answer is incorrect a message showing the correct answer should be displayed.  
 # The program must use a function that accomplishes part of the needed tasks.
+
+import random
+def math_quiz():
+    num1 = random.randint(100, 999)
+    num2 = random.randint(100, 999)
+
+    print(f"   {num1}")
+    print(f"+  {num2}")
+    print("------")
+
+    user_answer = int(input("Your answer: ")) 
+    correct_answer = num1 + num2 
+    if user_answer == correct_answer:
+        print("Congratulations! You got it right!")
+    else:
+        print("Sorry, the right answer is {correct_answer}.")
+if __name__ == '__main__': 
+        print("Welcome to the Simple Math Quiz!")
+        math_quiz()
